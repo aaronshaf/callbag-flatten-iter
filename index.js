b374df3f8846688dbb5bcb0267cbd67cf521e8de
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-07-30 09:06:28 +0200

    Remove unnecessary proxying function

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const fromIter = require("callbag-from-iter");
 const flatten = require("callbag-flatten");
 const map = require("callbag-map");
 
-const flattenIter = source => flatten(map(i => fromIter(i))(source));
+const flattenIter = source => flatten(map(fromIter)(source));
 
 module.exports = flattenIter;
